4124478d71e1870961a004b6f5046cd362582368
Hillary Prosper Wahua <hillaryprosperwahua@gmail.com>
2025-10-16 16:04:28 -0700

    Initial commit

---
+++ app/contact/page.jsx
@@ -0,0 +1,13 @@
+export default function Contact() {
+  return (
+    <section className="contact">
+      <h2>Contact Us</h2>
+      <p>We’d love to hear from you! Get in touch for inquiries or orders.</p>
+      <ul>
+        <li>📱 WhatsApp: <a href="https://wa.me/234XXXXXXXXXX" target="_blank">+234 XXX XXX XXXX</a></li>
+        <li>📸 Instagram: <a href="https://instagram.com/diamondapparell" target="_blank">@diamondapparell</a></li>
+        <li>📧 Email: <a href="mailto:info@diamondapparell.com">info@diamondapparell.com</a></li>
+      </ul>
+    </section>
+  );
+}
